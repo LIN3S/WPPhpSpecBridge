4abfb7712091bbb831a26b5b10b2aaa3e4117279
benatespina <benatespina@gmail.com>
2015-09-22 13:48:31 +0200

    Added WPML global functions

---
+++ src/Wpml.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the WPPhpSpecBridge library.
+ *
+ * Copyright (c) 2015 LIN3S <info@lin3s.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Beñat Espiña <benatespina@gmail.com>
+ */
+
+namespace
+{
+    function icl_t()
+    {
+        return true;
+    }
+
+    function icl_register_string()
+    {
+        return true;
+    }
+
+    function __($key)
+    {
+        return $key;
+    }
+}
